e3dad0ff04ef009ed1117f92d49d0125a5eedf7b
skyh24 <skyhighfeng@gmail.com>
2022-12-16 22:50:52 +0800

    add USDPaymaster

---
+++ contracts/IOracle.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+
+interface IOracle {
+    function getTokenValueOfEth(uint256 ethOutput) external view returns (uint256 tokenInput);
+}
